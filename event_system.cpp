faa2130fac2052dc139137857a668e3e93d19d78
Dotsially <mateu9999@gmail.com>
2025-11-16 14:46:59 +0100

    Event system added. Fix names later

--- event_system.cpp
+++ event_system.cpp
@@ -5,7 +5,13 @@
 
 enum class EventType {
     None,
-    PacketReceived
+    BlockBreak,
+    BlockPlaced,
+    PacketClientSend,
+    PacketServerSend,
+    ClientEntityCreate, //TODO FIX THESE DAMN NAMES
+    HandshakeEntityCreate, //TODO FIX THERE DAMN NAMES
+    EntityAction,
 };
 
 struct Event {
@@ -13,13 +19,54 @@ struct Event {
     void* data = nullptr;  // Optional payload
 };
 
-struct PacketReceivedEvent {
+//kinda stupid to do this on the framework...
+struct BlockBreakEvent {
+    glm::ivec3 position;
+};
+
+struct BlockPlacedEvent{
+    glm::ivec3 position;
+    uint32_t id;
+    uint32_t flags;
+};
+
+struct PacketClientSendEvent{
     uint8_t* data;
     size_t size;
+    uint8_t packetType;
+};
+
+struct PacketServerSendEvent{
+    uint8_t* data;
+    size_t size;
+    size_t size2; //For extra data if neccessary??
+    uint32_t clientID;
+    uint8_t packetType;
+};
+
+struct ClientEntityCreateEvent{
+    uint32_t clientID;
+};
+
+struct HandshakeEntityCreateEvent{
+    uint8_t* data;
+    uint32_t entityCount;
+};
+
+struct EntityActionEvent{
+    uint32_t clientID;
+    uint32_t actionCode;
+    glm::ivec3 position;
 };
 
 struct EventSystem {
     using Listener = std::function<void(const Event&)>;
+
+    static EventSystem& Instance() {
+        static EventSystem instance = {};         
+        return instance;
+    }
+
     std::unordered_map<EventType, std::vector<Listener>> listeners;
     
     void Subscribe(EventType type, Listener listener) {
